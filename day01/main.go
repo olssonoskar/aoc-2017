41a96b0ea72a48f1c0c4150d57a7be2b83cafbbb
Oskar Olsson <oskar94olsson@gmail.com>
2020-04-13 11:33:07 +0200

    day1

--- day01/main.go
+++ day01/main.go
@@ -1,5 +1,43 @@
-package aoc2017
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	dat, err := ioutil.ReadFile("day1.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(dat)
+	inputLength := len(input)
+	
+	sum := review(input, 1)
+	fmt.Println("part 1 =", sum)
 
+	sum = review(input, inputLength / 2)
+	fmt.Println("part 2 =", sum)
+}
+
+func parseInt(number string) int {
+	val, err := strconv.Atoi(number)
+	if err != nil {
+		panic(err)
+	}
+	return val
+}
+
+func review(input string, offset int) (sum int) {
+	sum = 0
+	inputLength := len(input)
+	for index := 0;  index < inputLength; index++ {
+		current := input[index]
+		refrence := input[(index + offset) % inputLength ]
+		if (current == refrence) {
+			sum += parseInt(string(current))
+		}
+	}
+	return
 }
