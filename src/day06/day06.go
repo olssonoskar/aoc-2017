7364d4be05db786c1d4d67c6c1cc2ac6ac0382c8
Oskar Olsson <oskar94olsson@gmail.com>
2021-08-20 15:32:36 +0200

    Day06

---
+++ src/day06/day06.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"util"
+)
+
+func main() {
+	input := strings.Split(util.GetInput("day06"), " ")
+	banks := make([]int, len(input))
+	for idx, val := range input {
+		num, _ := strconv.Atoi(val)
+		banks[idx] = num
+	}
+	total, loop := findDuplicate(banks)
+	println("Part 1 = " + fmt.Sprint(total))
+	println("Part 2 = " + fmt.Sprint(loop))
+}
+
+func findDuplicate(banks []int) (int, int) {
+	iter := 0
+	seenBanks := make(map[string]int)
+	seenBanks[toString(banks)] = 0
+	for {
+		iter++
+		banks = distribute(banks)
+		result := toString(banks)
+		if seenAt, ok := seenBanks[result]; ok {
+			return iter, iter - seenAt
+		}
+		seenBanks[result] = iter
+	}
+}
+
+func distribute(banks []int) []int {
+	idx, blocks := blocksAtMax(banks)
+	banks[idx] = 0
+	for blocks > 0 {
+		idx++
+		banks[idx%len(banks)]++
+		blocks--
+	}
+	return banks
+}
+
+func blocksAtMax(banks []int) (int, int) {
+	indexOfMax := -1
+	max := 0
+	for idx, block := range banks {
+		if block > max {
+			max = block
+			indexOfMax = idx
+		}
+	}
+	return indexOfMax, max
+}
+
+func toString(banks []int) string {
+	res := ""
+	for _, block := range banks {
+		current := fmt.Sprint(block)
+		res += current + ","
+	}
+	return res[:len(res)-1]
+}
